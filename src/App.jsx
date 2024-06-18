e66ffbe7202ea65650e667902af34ec504cba438
Shashika17 <shashigakavinda@gmail.com>
2024-06-19 00:13:15 +0530

    added pages main functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import Blogs from "./pages/Blogs";
+import Contacts from "./pages/Contacts";
+import Research from "./pages/Research";
+import Resume from "./pages/Resume";
+
 const App = () => {
   return <div>App</div>;
 };
