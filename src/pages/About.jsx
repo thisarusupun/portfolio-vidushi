b895fab88fc2a7d16ec57d8a047d53a1bf6b2f92
denverma7 <126649187+denverma7@users.noreply.github.com>
2024-06-21 09:58:17 +0500

    Updated Pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <p>About Me</p>
+                <p><span>Know</span>Me More</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
