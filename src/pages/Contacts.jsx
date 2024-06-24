0edf1dac63814d0360b342d11b1118274a745f9f
Shashika17 <shashigakavinda@gmail.com>
2024-06-24 18:41:08 +0530

    form funtion added

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -13,7 +13,7 @@ function Contacts() {
           <div className='absolute w-[39.23vw] h-[697px] left-[50.58vw] bg-transparent pt-[80px]'>
             
             <div className='w-[39.23vw] h-[697px] bg-[#EDF2FF]'>
-             <form>
+             <form action="https://getform.io/f/rbeqgpvb" method="POST">
                 <p className='font-[poppins] font-medium pt-[30px] text-left pl-[2.196vw] text-[#00113D] opacity-1 h-[23px] pb-[20px]'>Name *</p> <br/>
              <div className='pl-[2.196vw]'>
                     <input
@@ -33,7 +33,7 @@ function Contacts() {
                     name="email"
 // value={formData.name}
 // onChange={handleChange}
-                    className="text-gray-700 border-black border h-[55px] w-[34.94vw]  text-[24px]"
+                    className="text-gray-700 border-black border h-[55px] w-[34.94vw]  text-[24px] z-100"
                     required/>
              </div><br/>
                   <p className=' font-[poppins] font-medium text-left pl-[2.196vw] text-[#00113D] opacity-1 h-[23px] pb-[20px]'>Subject *</p><br/>
@@ -70,16 +70,16 @@ function Contacts() {
 
 
 
-<div className='relative top-[272.09px] left-[10.175vw]'>
+<div className='relative top-[272.09px] left-[10.175vw] w-[38vw]'>
 
 <div className='flex flex-row space-x-[0.75vw]'>
 <svg xmlns="http://www.w3.org/2000/svg" width="28.375" height="19.154" viewBox="0 0 28.375 19.154">
   <path id="Icon_zocial-email" data-name="Icon zocial-email" d="M.072,21.525V5.764q0-.027.082-.52L9.43,13.18.181,22.073a2.319,2.319,0,0,1-.109-.547ZM1.3,4.15a1.179,1.179,0,0,1,.465-.082H26.75a1.549,1.549,0,0,1,.493.082l-9.3,7.962-1.231.985-2.435,2-2.435-2-1.231-.985Zm.027,18.99,9.331-8.948,3.612,2.928,3.612-2.928,9.331,8.948a1.314,1.314,0,0,1-.465.082H1.768a1.239,1.239,0,0,1-.438-.082Zm17.786-9.96,9.249-7.935a1.633,1.633,0,0,1,.082.52V21.525a2.1,2.1,0,0,1-.082.547Z" transform="translate(-0.072 -4.068)" fill="#fff"/>
 </svg>
-<p className='text-white poppins-thin text-[16px]'>ebbandara199@gmail.com</p>
+<p className='text-white poppins-thin text-[16px] w-[30vw]'>ebbandara199@gmail.com</p>
 </div>
 
-<div className='flex flex-row space-x-[0.75vw] pt-[25px] pb-[27px]'>
+<div className='flex flex-row space-x-[0.75vw] pt-[25px] pb-[27px] w-[30vw]'>
 <svg xmlns="http://www.w3.org/2000/svg" width="21.174" height="21.174" viewBox="0 0 21.174 21.174">
   <path id="Icon_material-local-phone" data-name="Icon material-local-phone" d="M8.758,13.664a17.82,17.82,0,0,0,7.752,7.752L19.1,18.828a1.17,1.17,0,0,1,1.2-.282,13.418,13.418,0,0,0,4.2.671,1.18,1.18,0,0,1,1.176,1.176V24.5A1.18,1.18,0,0,1,24.5,25.674a20,20,0,0,1-20-20A1.18,1.18,0,0,1,5.676,4.5H9.793A1.18,1.18,0,0,1,10.97,5.676a13.363,13.363,0,0,0,.671,4.2,1.181,1.181,0,0,1-.294,1.2L8.758,13.664Z" transform="translate(-4.5 -4.5)" fill="#fff"/>
 </svg>
