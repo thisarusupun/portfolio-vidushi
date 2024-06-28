5d4afb093c315641232ec90e0d575cec43907449
Thisaru Supun <thisarusupun1314@gmail.com>
2024-06-29 00:57:45 +0530

    updated

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,32 +1,59 @@
-import React from 'react';
-import '../styles/styles.css';
+import React from "react";
+import "../styles/styles.css";
 
 function Blogs() {
   return (
-    <div>
-      <div className="bg-[url('src/Assests/images/blog.png')] h-[1425px] md:h-[782px]">
-        <p className='text-[#90F2FF] text-center pt-[120px] text-[16px] poppins-thin font-normal'>Blogs</p>
-        <div className="h-[71px] pt-[10px]">
-          <p className='text-[#90F2FF] text-[50px] poppins-extrathin text-center'>
-            <span className='font-bold'>My</span> Blogs
-          </p>
+    <div
+      name="blogs"
+      className="bg-[url('src/Assests/images/blog.png')] h-[1425px] md:h-[782px]"
+    >
+      <p className="text-[#90F2FF] text-center pt-[120px] text-[16px] poppins-thin font-normal">
+        Blogs
+      </p>
+      <div className="h-[71px] pt-[10px]">
+        <p className="text-[#90F2FF] text-[50px] poppins-extrathin text-center">
+          <span className="font-bold">My</span> Blogs
+        </p>
 
-          <div className='flex flex-col md:flex-row pt-[80px] space-y-5 md:space-y-0 md:space-x-[1.171vw] justify-center'>
-            <div className='rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm'>
-              <img src="src\Assests\images\blog1.png" alt="blog 01 image" className="w-full h-[250px] object-cover"/>
-              <p className='absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]'>Blog 01</p>
-              <p className='absolute text-white poppins-thin top-[317px] pl-[1.46vw] opacity-100 h-[23px]'>Read more</p>
-            </div>
-            <div className='rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm mx-auto'>
-              <img src="src\Assests\images\blog2.png" alt="blog 02 image" className="w-full h-[250px] object-cover"/>
-              <p className='absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]'>Blog 02</p>
-              <p className='absolute text-white poppins-thin top-[317px] pl-[1.46vw] opacity-100'>Read more</p>
-            </div>
-            <div className='rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm mx-auto'>
-              <img src="src\Assests\images\blog3.png" alt="blog 03 image" className="w-full h-[250px] object-cover"/>
-              <p className='absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]'>Blog 03</p>
-              <p className='absolute text-[#FFFFFF] poppins-thin top-[317px] pl-[1.46vw] opacity-100'>Read more</p>
-            </div>
+        <div className="flex flex-col md:flex-row pt-[80px] space-y-5 md:space-y-0 md:space-x-[1.171vw] justify-center">
+          <div className="rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm">
+            <img
+              src="src\Assests\images\blog1.png"
+              alt="blog 01 image"
+              className="w-full h-[250px] object-cover"
+            />
+            <p className="absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]">
+              Blog 01
+            </p>
+            <p className="absolute text-white poppins-thin top-[317px] pl-[1.46vw] opacity-100 h-[23px]">
+              Read more
+            </p>
+          </div>
+          <div className="rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm mx-auto">
+            <img
+              src="src\Assests\images\blog2.png"
+              alt="blog 02 image"
+              className="w-full h-[250px] object-cover"
+            />
+            <p className="absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]">
+              Blog 02
+            </p>
+            <p className="absolute text-white poppins-thin top-[317px] pl-[1.46vw] opacity-100">
+              Read more
+            </p>
+          </div>
+          <div className="rounded-sm hover:scale-105 duration-200 relative h-[358px] w-full md:w-[25.76vw] bg-black border border-white opacity-100 backdrop-blur-sm mx-auto">
+            <img
+              src="src\Assests\images\blog3.png"
+              alt="blog 03 image"
+              className="w-full h-[250px] object-cover"
+            />
+            <p className="absolute text-white pl-[1.46vw] poppins-thin font-bold text-[16px] text-left top-[289px]">
+              Blog 03
+            </p>
+            <p className="absolute text-[#FFFFFF] poppins-thin top-[317px] pl-[1.46vw] opacity-100">
+              Read more
+            </p>
           </div>
         </div>
       </div>
--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function Research() {
   return (
-    <div>
-      <div className="bg-[url('src/Assests/images/researchs.png')] bg-[100vw] h-[800px] md:h-[605px] overflow-hidden">
-      <p className='relative text-[#90F2FF] text-center text-[16px] pt-[60px] md:pt-[120px] poppins-thin'>Researchers</p>
-      <p className='relative poppins-extrathin text-[#90F2FF] text-center text-[50px] pt-[10px]'>
-        <span className='font-bold'>A Summary of</span> My Researchers</p>
-        <div className='relative bg-transparent top-[150px] md:top-[110px] h-[151px] w-[98.65vw]'>
-              <div className='flex flex-col md:flex-row w-[98.65vw]] bg-transparent'>
-                <div className='relative bg-white w-[0px] md:w-[0.073vw] h-[0px] md:h-[151px] left-[16.91vw]'></div>
-                <div className='relative h-[55px]  w-[15vw] md:w-[4.02vw] bg-black top-[-120px] md:top-[22px] left-[40.73vw]  md:left-[15.73vw]'>
-                  <p className='relative top-[10px] text-center poppins-thin font-bold text-[25px] text-white'>01.</p>
-                </div>
-                <div className='relative left-[19.50vw] md:left-[17.50vw] top-[-100px] md:top-[27px] w-[57.39vw] h-[55px] bg-transparent'>
-                  <div className=' relative bg-white h-[0.073vh] md:h-[0vh] w-[57vw] md:w-[0vw] bottom-[10px]'></div>
-                  <p className='text-white text-[18px] poppins-thin font-medium md:text-left text-center'>Full keratin hair growth oil portion: increasing the hair thickness, stringiness and depth of the follicles with peppermint extract</p>
-                </div>
-                <div className='flex md:justify-start justify-center md:items-start items-center w-[57.39vw] h-[55px] bg-transparent'>
-                <div className='relative top-[70px] md:top-[22px]'>
-                  <img src="src\Assests\images\qr.png" className="relative bg-black left-[22.411vw] w-[76px] h-[75px] md:top-[22px]"/>
-                  <p className='text-white relative  left-[22.211vw] top-[32px] poppins-thin font-bold text-[16px]'>Ref. No: 119</p>
-                  </div>
-                  </div>
-                
-              </div>
+    <div
+      name="researchers"
+      className="bg-[url('src/Assests/images/researchs.png')] bg-[100vw] h-[800px] md:h-[605px] overflow-hidden"
+    >
+      <p className="relative text-[#90F2FF] text-center text-[16px] pt-[60px] md:pt-[120px] poppins-thin">
+        Researchers
+      </p>
+      <p className="relative poppins-extrathin text-[#90F2FF] text-center text-[50px] pt-[10px]">
+        <span className="font-bold">A Summary of</span> My Researchers
+      </p>
+      <div className="relative bg-transparent top-[150px] md:top-[110px] h-[151px] w-[98.65vw]">
+        <div className="flex flex-col md:flex-row w-[98.65vw]] bg-transparent">
+          <div className="relative bg-white w-[0px] md:w-[0.073vw] h-[0px] md:h-[151px] left-[16.91vw]"></div>
+          <div className="relative h-[55px]  w-[15vw] md:w-[4.02vw] bg-black top-[-120px] md:top-[22px] left-[40.73vw]  md:left-[15.73vw]">
+            <p className="relative top-[10px] text-center poppins-thin font-bold text-[25px] text-white">
+              01.
+            </p>
+          </div>
+          <div className="relative left-[19.50vw] md:left-[17.50vw] top-[-100px] md:top-[27px] w-[57.39vw] h-[55px] bg-transparent">
+            <div className=" relative bg-white h-[0.073vh] md:h-[0vh] w-[57vw] md:w-[0vw] bottom-[10px]"></div>
+            <p className="text-white text-[18px] poppins-thin font-medium md:text-left text-center">
+              Full keratin hair growth oil portion: increasing the hair
+              thickness, stringiness and depth of the follicles with peppermint
+              extract
+            </p>
+          </div>
+          <div className="flex md:justify-start justify-center md:items-start items-center w-[57.39vw] h-[55px] bg-transparent">
+            <div className="relative top-[70px] md:top-[22px]">
+              <img
+                src="src\Assests\images\qr.png"
+                className="relative bg-black left-[22.411vw] w-[76px] h-[75px] md:top-[22px]"
+              />
+              <p className="text-white relative  left-[22.211vw] top-[32px] poppins-thin font-bold text-[16px]">
+                Ref. No: 119
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Research
+export default Research;
