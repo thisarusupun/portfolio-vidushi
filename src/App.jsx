bc85504420f84328bac8c6d8c42e329a14cf1594
Shashika17 <shashigakavinda@gmail.com>
2024-06-30 15:21:30 +0530

    solved navbar problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Contacts from "./pages/Contacts";
 import Research from "./pages/Research";
 import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return <div>
+<Navbar className="z-40"/> 
 <Resume/>
 <Blogs/>
 <Research/>
-<Contacts/>
+<Contacts/> 
+
   
     
     </div>;
